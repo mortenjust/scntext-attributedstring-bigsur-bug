d9c2d3b37140e47e724d353e8ac910b0bca4c3d2
Morten Just <morten.just@genstart.dk>
2020-12-08 07:46:17 +0100

    Initial Commit

---
+++ scnkittextnode/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  scnkittextnode
+//
+//  Created by Morten Just on 12/8/20.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
